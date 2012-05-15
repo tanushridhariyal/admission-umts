f9ffda1f8fa70147addcc4562d45afb4abcb2e0b
quentin.grosperrin@gmail.com <quentin.grosperrin@gmail.com@6f81cf2f-9475-729e-ec91-cbdcc5434db1>
2012-05-15 10:02:33 +0000

    ajout de la base des fichiers sources

---
+++ trunk/src/BaseStation.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <set>
+#include "User.h"
+
+class BaseStation
+{
+public:
+	BaseStation(void);
+	~BaseStation(void);
+	float computeIncreaseEstimation (User user);
+	bool isAdmissible(User user);
+
+	//Accessors
+	const std::set<User>& getUsersList(void) const;
+	const User& getUserAt(int id) const;
+	const float& getPilotPower(void) const;
+	const float& getBandwidth(void) const;
+	const float& getSnrTarget(void) const;
+	const float& getBitRate(void) const;
+	const float& getNoisePower(void) const;
+	const float& getOrthoFactor(void) const;
+	const float& getBaseStationHeight(void) const;
+	const float& getGain(void) const;
+	const float& getFrequency(void) const;
+	const float& getTotalTransmittedPower(void) const;
+	const float& getThresholdPower(void) const;
+
+	//Mutators
+	void setThresholdPower(float thresholdPower);
+
+private:
+	std::set<User> _usersList;
+
+	float _pilotPower;
+	float _bandwidth;
+	float _snrTarget;
+	float _bitRate;
+	float _noisePower;
+	float _orthoFactor;
+	float _baseStationHeight;
+	float _gain;
+	float _frequency;
+	float _totalTransmittedPower;
+	float _thresholdPower;
+};
+
