426a0cd5b6ef016100e7f32d55485c17c1b6b858
quentin.grosperrin@gmail.com <quentin.grosperrin@gmail.com@6f81cf2f-9475-729e-ec91-cbdcc5434db1>
2012-05-21 17:01:32 +0000

    compil ok !

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -1,10 +1,27 @@
 #include "admission_umts.h"
 #include <QtGui/QApplication>
 
+#include <iostream>
+#include "User.h"
+#include "BaseStation.h"
+
+using namespace std;
+
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
+    
+	BaseStation base1(32,5000000,5,64,-100,0.4,30,0,2100000000,43,25);
+	
+	int a(2),b(2);
+    cout << "Valeur de a : " << a << endl;
+    cout << "Valeur de b : " << b << endl;
+    //b = ajouteDeux(a);                     //Appel de la fonction
+    cout << "Valeur de a : " << a << endl;
+    cout << "Valeur de b : " << b << endl;
+
+	//BaseStation base1 = 
+	QApplication app(argc, argv);
 	admission_umts w;
 	w.show();
-	return a.exec();
+	return app.exec();
 }
