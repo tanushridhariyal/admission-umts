30c9e4e16228c2b2468c0dc6bc7711f241c48920
quentin.grosperrin@gmail.com <quentin.grosperrin@gmail.com@6f81cf2f-9475-729e-ec91-cbdcc5434db1>
2012-05-21 15:17:43 +0000

    ajout des fichiers Qt

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,10 @@
-#include <iostream>
-#include "User.h"
-#include "BaseStation.h"
+#include "admission_umts.h"
+#include <QtGui/QApplication>
 
-using namespace std;
-
-int main()
+int main(int argc, char *argv[])
 {
-    
-	BaseStation base1(32,5000000,5,64,-100,0.4,30,0,2100000000,43,25);
-
-
-
-	
-	int a(2),b(2);
-    cout << "Valeur de a : " << a << endl;
-    cout << "Valeur de b : " << b << endl;
-    b = ajouteDeux(a);                     //Appel de la fonction
-    cout << "Valeur de a : " << a << endl;
-    cout << "Valeur de b : " << b << endl;
-
-	BaseStation base1 = 
-
-    return 0;
+	QApplication a(argc, argv);
+	admission_umts w;
+	w.show();
+	return a.exec();
 }
-
