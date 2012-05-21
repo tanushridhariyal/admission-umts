63c9dba87e9d9a96a2b9ee00737c5e565616de29
liquidsboy@gmail.com <liquidsboy@gmail.com@6f81cf2f-9475-729e-ec91-cbdcc5434db1>
2012-05-21 14:27:30 +0000

    Salai

---
+++ trunk/src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "User.h"
+#include "BaseStation.h"
+
+using namespace std;
+
+int main()
+{
+    
+	BaseStation base1(32,5000000,5,64,-100,0.4,30,0,2100000000,43,25);
+
+
+
+	
+	int a(2),b(2);
+    cout << "Valeur de a : " << a << endl;
+    cout << "Valeur de b : " << b << endl;
+    b = ajouteDeux(a);                     //Appel de la fonction
+    cout << "Valeur de a : " << a << endl;
+    cout << "Valeur de b : " << b << endl;
+
+	BaseStation base1 = 
+
+    return 0;
+}
+
